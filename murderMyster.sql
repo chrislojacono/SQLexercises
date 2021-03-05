180fa3212222ee566bafa41710fdb9af586a3afc
Chris LoJacono <clojacono92@gmail.com>
2021-03-04 20:33:02 -0600

    murder mystery

---
+++ murderMyster.sql
@@ -0,0 +1,7 @@
+-- Security footage shows that there were 2 witnesses. The first witness lives at the last house on "Northwestern Dr". The second witness, named Annabel, lives somewhere on "Franklin Ave".
+
+-- ANnabel Miller
+--id 16371
+-- license id 490173
+-- address 103 franklin ave
+-- ssn 318771143
