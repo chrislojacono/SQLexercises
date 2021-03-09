2243c9bfcfbc80983f9270b7f2cd0e40dcc23b9d
Chris LoJacono <clojacono92@gmail.com>
2021-03-08 18:57:42 -0600

    more notes

--- settingUpDatabases.sql
+++ settingUpDatabases.sql
@@ -17,4 +17,13 @@ ALTER TABLE dbo.Table_1 ADD CONSTRAINT
 GO
 
 ------------------------------------------------------------------------------
-
+CREATE TABLE Loaves
+	(
+	Id int NOT NULL,
+	Size int NOT NULL,
+	Type varchar(40) NOT NULL,
+	Price decimal(18, 2) NOT NULL,
+	WeightInOunces int NOT NULL,
+	Sliced bit NOT NULL DEFAULT 0,
+	CreatedDate datetime NOT NULL DEFAULT getdate()
+	) 
